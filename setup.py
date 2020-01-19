d9116c0d79fa349b4fa4228b59e4403ea566e815
jsunishka <60058295+jsunishka@users.noreply.github.com>
2020-01-19 15:39:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python package for TOPSIS implementation using Vector Normalization.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    #url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jsunishka/TOPSIS_package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
